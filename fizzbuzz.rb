c7ffb1ee659bf1cd1e67d9fcd0d47d3267de4827
Tyrell Gordon <gordontyrell1993@gmail.com>
2018-07-02 01:19:23 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,11 @@
 def fizzbuzz(int)
     
   if int % 5 == 0 && int % 3 == 0
-    "FizzBuzz
-  
-  
+    "FizzBuzz"
   elsif int % 3 == 0 
     "Fizz" 
   elsif int % 5 == 0 
     "Buzz"
-  elsif int % 5 == 0 && int % 3 == 0
-    "FizzBuzz"
   else
     nil 
   end
